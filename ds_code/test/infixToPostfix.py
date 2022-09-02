76d89b599f2a2619b5dbb1549039c5d722fe0976
Williamlambert <williamlambert@whu.edu.cn>
2022-09-02 15:59:33 +0800

    stack's application

--- ds_code/test/infixToPostfix.py
+++ ds_code/test/infixToPostfix.py
@@ -1,8 +1,10 @@
+from pickletools import StackObject
 import sys,os
 sys.path.append("ds_code/basic/")
 # print(sys.path)
 # print(os.getcwd())
 
+from stack import Stack
 def infixToPostfix(infixexpr):
     opstack=Stack()
     postfixList=[]
@@ -34,6 +36,27 @@ def infixToPostfix(infixexpr):
         postfixList.append(opstack.pop())
 
     return " ".join(postfixList)
+res = infixToPostfix("3*4+5*6")
+print(res)
 
-print(infixToPostfix("A*B+C*D"))
-list("A*B+C*D")
+
+def evalPostfix(postfix):
+    digits = Stack()
+    for i,token in enumerate(postfix):
+        if token in "0123456789":
+            digits.push(float(token))
+        elif token in "*/+-":
+            rt=digits.pop()
+            lt=digits.pop()
+            if token == "*":
+                temp_res=lt*rt
+            elif token == "/":
+                temp_res=lt*rt
+            elif token == "+":
+                temp_res=lt+rt
+            elif token == "-":
+                temp_res=lt-rt
+            digits.push(temp_res)
+    return digits.pop()
+
+print(evalPostfix(res))
